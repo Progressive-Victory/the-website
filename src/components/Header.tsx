20762d1bb1eaee2e55b295f06931b284c89a9f55
naterpotatoer <nate.garza@hotmail.com>
2025-01-13 22:03:33 -0800

    :lipstick: updates nav w Blog links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,23 +2,31 @@
 import { Logo } from "./Logo";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import Hamburger from "./Hamburger";
+
 export function Header() {
   return (
-    <div className="sticky top-0 left-0 right-0 flex flex-row items-center justify-between w-full gap-x-4 px-12 py-4 z-10 bg-steel-blue">
-      <div className="flex flex-row items-center justify-start gap-x-4">
-        <Logo className="w-12 h-12 bg-white p-1 rounded-full" />
-        <h1 className="text-2xl font-bold text-white">Progressive Victory</h1>
-      </div>
-      <Button className="text-xl bg-jasper px-4 py-2 rounded-full text-white font-bold hover:bg-white hover:text-black transition duration-300 ease-in-out">
-        <Link
-          href="#"
-        >
-          Donate
+    <header className="sticky top-0 left-0 right-0 w-full p-5 z-10">
+      <nav className="flex flex-wrap justify-between gap-1">
+        <Link href="/" className="flex items-center pr-2 rounded-md">
+          <Logo className="w-12 h-12 p-1 rounded-md" />
+          <span className="text-xl font-bold">Progressive Victory</span>
         </Link>
-      </Button>
-      <Button>
-        Test
-      </Button>
-    </div>
+        <div className="flex flex-wrap gap-1 items-center">
+          <Link href="#">
+            <Button variant="ghost">Donate</Button>
+          </Link>
+          <Link href="/blog">
+            <Button variant="ghost">Blog</Button>
+          </Link>
+          <Link href="/blog/post">
+            <Button variant="ghost">Post</Button>
+          </Link>
+          <div className="ml-auto mr-5 md:hidden">
+            <Hamburger />
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 }
