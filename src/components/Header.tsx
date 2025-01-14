1a3ea03f093a4e8cfb304bbf67710bf680b359f4
naterpotatoer <nate.garza@hotmail.com>
2025-01-13 18:06:54 -0800

    :sparkles: adds shadcn dep

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Logo } from "./Logo";
 import Link from "next/link";
+import { Button } from "./ui/button";
 export function Header() {
   return (
     <div className="sticky top-0 left-0 right-0 flex flex-row items-center justify-between w-full gap-x-4 px-12 py-4 z-10 bg-steel-blue">
@@ -8,12 +9,16 @@ export function Header() {
         <Logo className="w-12 h-12 bg-white p-1 rounded-full" />
         <h1 className="text-2xl font-bold text-white">Progressive Victory</h1>
       </div>
-      <Link
-        href="#"
-        className="text-xl bg-jasper px-4 py-2 rounded-full text-white font-bold hover:bg-white hover:text-black transition duration-300 ease-in-out"
-      >
-        Donate
-      </Link>
+      <Button className="text-xl bg-jasper px-4 py-2 rounded-full text-white font-bold hover:bg-white hover:text-black transition duration-300 ease-in-out">
+        <Link
+          href="#"
+        >
+          Donate
+        </Link>
+      </Button>
+      <Button>
+        Test
+      </Button>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,38 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    fontFamily: {
-      sans: ["Inter", "sans-serif"],
-      serif: ["Merriweather", "serif"],
-    },
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        "steel-blue": "#4483C7",
-        jasper: "#d65e43",
-        payne: "586575",
-        prussian: "#182B41",
-      },
-    },
+  	fontFamily: {
+  		sans: [
+  			'Inter',
+  			'sans-serif'
+  		],
+  		serif: [
+  			'Merriweather',
+  			'serif'
+  		]
+  	},
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)',
+  			'steel-blue': '#4483C7',
+  			jasper: '#d65e43',
+  			payne: '586575',
+  			prussian: '#182B41'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
