8160aef143f1b2160d4b1d80dd6e732e870d5864
naterpotatoer <nate.garza@hotmail.com>
2025-01-28 16:48:28 -0800

    :sparkles: adds double effect on blog title

---
+++ src/app/blog/BlogHeader.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Logo } from "@/components/Logo";
+
+export default function BlogHeader() {
+  function DoubleTextEffect(
+    text: string = "Test",
+    upperColor: string = "#09223a",
+    lowerColor: string = "#4483C7"
+  ) {
+    const words = text.split(" ");
+    return (
+      <div>
+        {words.map((word, index) => (
+          <div key={index} className="inline-block relative">
+            <span
+              className="tracking-wide absolute bottom-1 right-1"
+              style={{ color: upperColor }}
+            >
+              {word}
+            </span>
+            <span className="tracking-wide" style={{ color: lowerColor }}>
+              {word}
+            </span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div className="bg-white grid grid-cols-2 py-5 text-4xl">
+      <h1 className="grid sm:text-5xl md:text-6xl lg:text-7xl text-right content-evenly font-extrabold text-[#09223a]">
+        {DoubleTextEffect("Progressive")}
+        {DoubleTextEffect("Victory")}
+        {DoubleTextEffect("Blog", "#4483C7", "#09223a")}
+      </h1>
+      <div className="inline-block relative">
+        <Logo className="w-60 absolute bottom-1 left-1" />
+        <Logo className="w-60" color="#4483C7" />
+      </div>
+    </div>
+  );
+}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,13 @@
 import BlogCard from "@/components/BlogCard";
 import { Header } from "@/components/Header";
+import BlogHeader from "./BlogHeader";
 
 export default function Home() {
   return (
     <div className="bg-steel-blue w-full">
       <Header />
       <div>
-        <h1 className="text-3xl md:text-5xl bg-white text-center">
-          Progressive Victory Blog
-        </h1>
+        <BlogHeader />
         <div className="bg-[#D4E6F5] p-10">
           <h2 className="text-2xl font-bold text-center p-10">Posts</h2>
           <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
