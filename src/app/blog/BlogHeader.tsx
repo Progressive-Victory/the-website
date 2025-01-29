50428cbd9efcac66a72563c3c3b05725299b009f
naterpotatoer <nate.garza@hotmail.com>
2025-01-28 17:13:25 -0800

    :lipstick: cleans up blog header

--- src/app/blog/BlogHeader.tsx
+++ src/app/blog/BlogHeader.tsx
@@ -13,7 +13,7 @@ export default function BlogHeader() {
         {words.map((word, index) => (
           <div key={index} className="inline-block relative">
             <span
-              className="tracking-wide absolute bottom-1 right-1"
+              className="tracking-wide absolute bottom-0.5 right-0.5"
               style={{ color: upperColor }}
             >
               {word}
@@ -29,14 +29,14 @@ export default function BlogHeader() {
 
   return (
     <div className="bg-white grid grid-cols-2 py-5 text-4xl">
-      <h1 className="grid sm:text-5xl md:text-6xl lg:text-7xl text-right content-evenly font-extrabold text-[#09223a]">
+      <h1 className="grid sm:text-5xl md:text-6xl lg:text-7xl text-right content-center font-extrabold text-[#09223a]">
         {DoubleTextEffect("Progressive")}
         {DoubleTextEffect("Victory")}
         {DoubleTextEffect("Blog", "#4483C7", "#09223a")}
       </h1>
-      <div className="inline-block relative">
-        <Logo className="w-60 absolute bottom-1 left-1" />
-        <Logo className="w-60" color="#4483C7" />
+      <div className="inline-block relative w-40 sm:w-50 md:w-60 lg:w-70">
+        <Logo className="absolute bottom-1 left-1" />
+        <Logo color="#4483C7" />
       </div>
     </div>
   );
