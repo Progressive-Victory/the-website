c88a9f4e97aa816057a978f966556da3e15d7ab0
naterpotatoer <nate.garza@hotmail.com>
2025-01-28 17:29:36 -0800

    :lipstick: cleans up blog header pt2

--- src/app/blog/BlogHeader.tsx
+++ src/app/blog/BlogHeader.tsx
@@ -9,7 +9,7 @@ export default function BlogHeader() {
   ) {
     const words = text.split(" ");
     return (
-      <div>
+      <p aria-label={text}>
         {words.map((word, index) => (
           <div key={index} className="inline-block relative">
             <span
@@ -23,20 +23,20 @@ export default function BlogHeader() {
             </span>
           </div>
         ))}
-      </div>
+      </p>
     );
   }
 
   return (
-    <div className="bg-white grid grid-cols-2 py-5 text-4xl">
-      <h1 className="grid sm:text-5xl md:text-6xl lg:text-7xl text-right content-center font-extrabold text-[#09223a]">
-        {DoubleTextEffect("Progressive")}
-        {DoubleTextEffect("Victory")}
-        {DoubleTextEffect("Blog", "#4483C7", "#09223a")}
-      </h1>
+    <div className="bg-white grid grid-cols-2 py-5">
+      <div className="grid text-2xl sm:text-4xl md:text-5xl lg:text-6xl text-right content-center font-extrabold text-[#09223a]">
+        {DoubleTextEffect("Progressive".toUpperCase())}
+        {DoubleTextEffect("Victory".toUpperCase())}
+        {DoubleTextEffect("Blog".toUpperCase(), "#4483C7", "#09223a")}
+      </div>
       <div className="inline-block relative w-40 sm:w-50 md:w-60 lg:w-70">
         <Logo className="absolute bottom-1 left-1" />
-        <Logo color="#4483C7" />
+        <Logo pColor="#4483C7" />
       </div>
     </div>
   );
