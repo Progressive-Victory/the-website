c05e3af886e5a9f8a88068280ec09e0f83733eac
k2468112 <k6325041@gmail.com>
2025-02-13 22:34:49 -0600

    just a bit more cleanup

--- src/api/oauth/discord.tsx
+++ src/api/oauth/discord.tsx
@@ -5,25 +5,14 @@
 import dotenv from 'dotenv'; 
 
 
-
+// more or less just copied this:https://github.com/alii/nextjs-discord-oauth/blob/main/pages/api/oauth.ts
 export async function getauth(codestr:string) {
-  // var code = request.query["code"]
     console.log("code")
     dotenv.config();  // Load environment variables from .env file 
     const app_id = process.env.OAUTH_DISCORD_APPID!;  
     const secret = process.env.OAUTH_DISCORD_CLIENT_SECRET!
     const host = process.env.HOST!
     var RETURN_VARS = {}
-     // params.append("client_id", apiKey)
-    // params.append("client_secret",secret)
-    // params.append("grant_type", "authorization_code")
-    // params.append( code)
-    // params.append("redirect_uri",`http://${ip}:3000/oauth/discord/`)
-    // console.log("params == ", params.toString());
-    // fetch(`https://discord.com/api/oauth2/token`, {
-    //   method: "POST",
-    //   body: params
-    // })
     const body = new URLSearchParams({
       client_id: app_id,
       client_secret: secret,
@@ -42,10 +31,8 @@ export async function getauth(codestr:string) {
     const me = await fetch("https://discord.com/api/users/@me", {
       headers: { Authorization: `${token_type} ${access_token}` },
     }).then((res) => {RETURN_VARS = res.json()});
-    // var params = new URLSearchParams()
 
-
-   
+    // TODO: implement cookies so logging in can actually work.
     return RETURN_VARS
 
     
