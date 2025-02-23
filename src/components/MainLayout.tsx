ff12aaef525e95bba97230c685eb153d0b2537f1
Seth Church <sethcdev@gmail.com>
2025-02-23 12:12:11 -0500

    make layout height consistent

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -2,9 +2,9 @@ import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
 export function MainLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div>
+        <div className="min-h-screen grid grid-rows-[min-content_1fr_min-content]">
             <Header />
-            {children}
+            <div>{children}</div>
             <Footer />
         </div>
     )
