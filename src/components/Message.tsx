6565f9810fb4214d39818072730af8ce8e4687d0
Seth Church <sethcdev@gmail.com>
2025-03-28 20:33:21 -0400

    add slight pop to cards

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -34,72 +34,74 @@ export function Message({
     const [clickedShare, setClickedShare] = useState<boolean>(false)
 
     return (
-        <motion.div
-            className="flex flex-col items-center justify-start w-fit xl:w-[30vw] p-4 bg-white rounded-md drop-shadow-lg shadow-xl my-2 max-w-xl"
-            initial={{ x: 100, opacity: 0 }} // Start position: off-screen to the right
-            animate={{ x: 0, opacity: 1 }} // End position: visible and on-screen
-            transition={{
-                duration: 0.5,
-                delay,
-                ease: 'easeOut',
-            }}
-        >
-            <div className="flex flex-row items-center justify-start gap-x-4 mr-auto">
-                <Image
-                    src={avatar}
-                    alt={username}
-                    className={`${avatarRounded ? 'rounded-full' : ''}`}
-                    width={38}
-                    height={38}
-                />
-                <p className="font-bold" style={{ color: nameColor }}>
-                    {username}
-                </p>
-            </div>
-            <p className="mt-2 text-left mr-auto">{text}</p>
-            {image && (
-                <div className="relative w-full h-[300px]">
+        <div className="hover:rotate-1 hover:scale-[102%] transition-transform ease-in-out">
+            <motion.div
+                className="flex flex-col items-center justify-start w-fit xl:w-[30vw] p-4 bg-white rounded-md shadow-xl my-2 max-w-xl"
+                initial={{ x: 100, opacity: 0 }} // Start position: off-screen to the right
+                animate={{ x: 0, opacity: 1 }} // End position: visible and on-screen
+                transition={{
+                    duration: 0.5,
+                    delay,
+                    ease: 'easeOut',
+                }}
+            >
+                <div className="flex flex-row items-center justify-start gap-x-4 mr-auto">
                     <Image
-                        src={image}
+                        src={avatar}
                         alt={username}
-                        className="rounded-lg object-cover mt-4"
-                        style={{ objectPosition: '0 20%' }}
-                        fill={true}
+                        className={`${avatarRounded ? 'rounded-full' : ''}`}
+                        width={38}
+                        height={38}
                     />
+                    <p className="font-bold" style={{ color: nameColor }}>
+                        {username}
+                    </p>
                 </div>
-            )}
-            <div className="flex flex-row items-center justify-end gap-x-4 mt-8 ml-auto">
-                <div
-                    className="w-6 h-6 group"
-                    onClick={() => setClickedBubble(!clickedBubble)}
-                >
-                    {clickedBubble ? (
-                        <SolidChatBubbleLeftRightIcon className="text-blue-500 grow" />
-                    ) : (
-                        <ChatBubbleLeftRightIcon className="text-black group-hover:scale-110 transition-all duration-100" />
-                    )}
+                <p className="mt-2 text-left mr-auto">{text}</p>
+                {image && (
+                    <div className="relative w-full h-[300px]">
+                        <Image
+                            src={image}
+                            alt={username}
+                            className="rounded-lg object-cover mt-4"
+                            style={{ objectPosition: '0 20%' }}
+                            fill={true}
+                        />
+                    </div>
+                )}
+                <div className="flex flex-row items-center justify-end gap-x-4 mt-8 ml-auto">
+                    <div
+                        className="w-6 h-6 group"
+                        onClick={() => setClickedBubble(!clickedBubble)}
+                    >
+                        {clickedBubble ? (
+                            <SolidChatBubbleLeftRightIcon className="text-blue-500 grow" />
+                        ) : (
+                            <ChatBubbleLeftRightIcon className="text-black group-hover:scale-110 transition-all duration-100" />
+                        )}
+                    </div>
+                    <div
+                        className="w-6 h-6 group"
+                        onClick={() => setClickedShare(!clickedShare)}
+                    >
+                        {clickedShare ? (
+                            <SolidArrowUpOnSquareIcon className="text-green-500 grow" />
+                        ) : (
+                            <ArrowUpOnSquareIcon className="text-black group-hover:scale-110 transition-all duration-100" />
+                        )}
+                    </div>
+                    <div
+                        className="w-6 h-6 group"
+                        onClick={() => setClickedHeart(!clickedHeart)}
+                    >
+                        {clickedHeart ? (
+                            <SolidHeartIcon className="text-red-500 grow" />
+                        ) : (
+                            <HeartIcon className="text-black group-hover:scale-110 transition-all duration-100" />
+                        )}
+                    </div>
                 </div>
-                <div
-                    className="w-6 h-6 group"
-                    onClick={() => setClickedShare(!clickedShare)}
-                >
-                    {clickedShare ? (
-                        <SolidArrowUpOnSquareIcon className="text-green-500 grow" />
-                    ) : (
-                        <ArrowUpOnSquareIcon className="text-black group-hover:scale-110 transition-all duration-100" />
-                    )}
-                </div>
-                <div
-                    className="w-6 h-6 group"
-                    onClick={() => setClickedHeart(!clickedHeart)}
-                >
-                    {clickedHeart ? (
-                        <SolidHeartIcon className="text-red-500 grow" />
-                    ) : (
-                        <HeartIcon className="text-black group-hover:scale-110 transition-all duration-100" />
-                    )}
-                </div>
-            </div>
-        </motion.div>
+            </motion.div>
+        </div>
     )
 }
