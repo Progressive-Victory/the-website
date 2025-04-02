724f567eb2281d6f28312303a55e11632ca65185
GreatWyrm <alecmusante@gmail.com>
2025-04-01 18:19:59 -0700

    Move calendars into a map, refactor based on feedback

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import { MainLayout } from '@/components/MainLayout'
 import { ClientCalendar } from '@/components/Calendar'
 export const metadata: Metadata = {
     title: 'PV - Events',
@@ -10,17 +11,28 @@ export const metadata: Metadata = {
         siteName: 'Progressive Victory',
         images: [{ url: `https://${process.env.SITE_URL}/images/banner.png` }],
     },
-}
-const baseString = "https://calendar.google.com/calendar/embed?title=Progressive%20Victory%20"
-const eventsCalendarSrc: string = "&src=c_c92baa7ee2cd5e3a35a54b8a558a7bfec3cb0067c2eeaf63db81331340801b8e%40group.calendar.google.com&color=%23009949";
-const otherEventsSrc: string = "&src=Y185NDhjMzI5OWU3OTQ2N2M3MjBkNWQzMTY0YjEzOGU4OGRiM2FjNTFiNmUxMmM0ZTc2ZjAzZjY0NThjMTA2OGYzQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23b09e00";
-const organzingMeetingsSrc: string = "&src=27d041938d95e549e529dcfa46f1989c1fde36c9acd31d07829872ef08e4961d%40group.calendar.google.com&color=%23ba7525";
-const skillTeamMeetingsSrc: string = "&src=0151b1ae27c001d91226349720dd486eb6c37a8c649807ad68d0aae28817d7c1%40group.calendar.google.com&color=%230099e1";
-// Embed options
-const standardParams = "&mode=week&wkst=2"
-
+} 
+// Map calendar src embed to their RGB color
+const calendarMap = new Map<string, string>([
+    ["27d041938d95e549e529dcfa46f1989c1fde36c9acd31d07829872ef08e4961d@group.calendar.google.com", "ba7525"],
+    ["c_c92baa7ee2cd5e3a35a54b8a558a7bfec3cb0067c2eeaf63db81331340801b8e@group.calendar.google.com", "009949"],
+    ["Y185NDhjMzI5OWU3OTQ2N2M3MjBkNWQzMTY0YjEzOGU4OGRiM2FjNTFiNmUxMmM0ZTc2ZjAzZjY0NThjMTA2OGYzQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20", "b09e00"],
+    ["0151b1ae27c001d91226349720dd486eb6c37a8c649807ad68d0aae28817d7c1@group.calendar.google.com", "0099e1"]
+]);
 export default function Events() {
-    return <ClientCalendar>{baseString + eventsCalendarSrc + otherEventsSrc + organzingMeetingsSrc + skillTeamMeetingsSrc + standardParams}</ClientCalendar>
+    let calendarUri = new URL("https://calendar.google.com/calendar/embed");
+    // build calendar string out of the base embed string plus their colors
+    for (let [key, value] of calendarMap) {
+        // src - calendar id, color - color that the events from that calendar show up as
+        // the %23 magic value is # encoded into the URL
+        calendarUri.searchParams.append("src", key);
+        calendarUri.searchParams.append("color", "#" + value);
+    }
+    // Misc embed options
+    calendarUri.searchParams.append("title", "Progressive Victory")
+    calendarUri.searchParams.append("mode", "week");
+    calendarUri.searchParams.append("wkst", "2");
+    return <MainLayout><ClientCalendar src={calendarUri.toString()}></ClientCalendar></MainLayout>
 }
 
 // Add functionality for filtering calendar events based on type or tag - Needs API access as far as I can tell
--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,20 +1,18 @@
 'use client'
 
 import { Frame } from '@/components/Frame'
-import { MainLayout } from '@/components/MainLayout'
 
-export function ClientCalendar({ children }: Readonly<{children: string}>) {
+export function ClientCalendar({ src }: Readonly<{src: string}>) {
     // Get client timezone and encode it to use in the URL
     const timezoneParameter: string = "&ctz=" + encodeURI(Intl.DateTimeFormat().resolvedOptions().timeZone);
     return (
-            <MainLayout>
                 <div className="bg-steel-blue w-full h-full flex flex-col items-center p-4 gap-y-10">
                     <div className="w-full bg-[#f0f4f9] rounded-lg py-4 mx-6">
                         <p className="text-center text-3xl font-black lg:text-5xl">
                             Progressive Victory Calendar
                         </p>
                         <Frame
-                            src={children + timezoneParameter}
+                            src={src + timezoneParameter}
                             className="mx-auto h-[800px] mt-4 w-[95%] bg-[#f0f4f9]"
                             type="calendar"
                             title="Calendar"
@@ -23,6 +21,5 @@ export function ClientCalendar({ children }: Readonly<{children: string}>) {
                         </Frame>
                     </div>
                 </div>
-            </MainLayout>
         )
 }
