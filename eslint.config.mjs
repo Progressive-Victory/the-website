6b165a7b97b365ca0a5eec135524ed402b188b32
Wyndolin Crook <wyncrook@protonmail.com>
2025-05-16 20:02:11 -0600

    bugfix: more lint fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
+import js from "@eslint/js/src/index.js";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
