3e57dd34392279c78c1da1b7831505b181ff21a6
William Bowen <bowenjw22@outlook.com>
2025-05-17 10:33:45 -0400

    package bump and eslint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,13 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js/src/index.js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+import {flatConfig as pluginNext}  from '@next/eslint-plugin-next';
 
 export default defineConfig([
-    globalIgnores(["**/wp-content/", "**/wp-includes/"]),
-    {
-    extends: compat.extends("next/core-web-vitals", "next/typescript"),
-}]);
+  globalIgnores([".config/*", "./src/components/wp-includes/**/*", "./src/components/wp-content/**/*"]),
+  pluginNext.coreWebVitals,
+  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  tseslint.configs.recommended,
+]);
