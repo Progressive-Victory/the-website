518ea2fdfe4930ab5b4dcd9b0fd3e693b14e27c6
Wyndolin Crook <wyncrook@protonmail.com>
2025-05-17 13:00:26 -0600

    Revert "bugfix: more lint fixes"
    This reverts commit 6b165a7b97b365ca0a5eec135524ed402b188b32.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,18 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
-import {flatConfig as pluginNext}  from '@next/eslint-plugin-next';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
 
 export default defineConfig([
   globalIgnores([".config/*", "./src/components/wp-includes/**/*", "./src/components/wp-content/**/*"]),
