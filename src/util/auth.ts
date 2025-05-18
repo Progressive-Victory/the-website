c57d405f11348fefe3966b4beff38d3e84e8d830
Hyrum Hammon <38766853+hhammon@users.noreply.github.com>
2025-05-17 23:37:50 -0600

    bugfix: Using new avatar values and processes to make avatars default when not available.

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -6,6 +6,7 @@ import { JWT } from 'next-auth/jwt'
 import { NextAuthOptions } from 'next-auth'
 import { IUser } from '@/models/User'
 import { IRole } from '@/models/Role'
+import { getDisplayAvatarURL } from './discord-rest'
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -13,28 +14,47 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.DISCORD_CLIENT_ID!,
             clientSecret: process.env.DISCORD_CLIENT_SECRET!,
             authorization:
-                'https://discord.com/oauth2/authorize?scope=identify+guilds+guilds.join+email',
+                'https://discord.com/oauth2/authorize?scope=identify+guilds+guilds.join+guilds.members.read+email',
             async profile(profile) {
+                // FIXME(hhammon) There may be a better way to do this, but if this is the way,
+                // an index needs to be created on the discordId key. Likely, we want this lookup
+                // anyway going forward so that data we store can be exposed through the session,
+                // though there's an argument that the current method of calling GET /api/user is
+                // always preferable, there may be a better solution even for the image going
+                // that route.
+
+                const user = await User.findOne({ discordId: profile.id })
+                    .select({
+                        discordUserAvatar: 1,
+                    })
+                    .lean()
+                    .exec()
+
+                console.log({
+                    user,
+                    keys: Object.keys(user || {}),
+                    values: Object.values(user || {}),
+                })
+
+                const image = await getDisplayAvatarURL(
+                    profile.id,
+                    user?.discordUserAvatar
+                )
+
                 return {
                     id: profile.id,
                     name: profile.username,
                     email: profile.email,
                     // Using long form here to adjust size of image
-                    image: `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}?size=512`,
+                    image,
                 }
             },
         }),
     ],
     callbacks: {
         // session call back assigns the discordId from the token to the session object
-        async session({
-            session,
-            token
-        }: {
-            session: Session
-            token: JWT
-        }) {
-            if(token.discordId) session.discordId = token.discordId as string
+        async session({ session, token }: { session: Session; token: JWT }) {
+            if (token.discordId) session.discordId = token.discordId as string
             return session
         },
         async jwt({
@@ -73,6 +93,7 @@ export const authOptions: NextAuthOptions = {
                         // Using long form here to adjust size of image
                         image: `https://cdn.discordapp.com/avatars/${eprofile.id}/${eprofile.avatar}?size=512`,
                         discordId: eprofile.id,
+                        discordUserAvatar: eprofile.avatar,
                     })
                     await newUser.save()
                 }
@@ -87,7 +108,7 @@ export const enum ResponseCode {
     Successful,
     Exception,
     NoSession,
-    InsufficientAccess
+    InsufficientAccess,
 }
 
 // Role checking utility function
@@ -96,7 +117,7 @@ const hasRequiredRoles = (user: IUser, requiredRoles: string[] = []) => {
     const userRoles = user.roles as IRole[]
     const roleStrs = userRoles.map((role: IRole) => role.name)
     if (!user || !user.roles || !Array.isArray(user.roles)) return false
-    return requiredRoles.every((role) => roleStrs.includes(role))   
+    return requiredRoles.every((role) => roleStrs.includes(role))
 }
 
 // utility function for checking the current session against an array of roles
@@ -112,22 +133,25 @@ export async function checkAuth(roles?: string[]): Promise<ResponseCode> {
     if (!session || !session.user) return ResponseCode.NoSession
 
     //if there are no required roles, then all auth requirements have been met.
-    if(!roles) return ResponseCode.Successful
+    if (!roles) return ResponseCode.Successful
 
     // Connect to database
     await dbConnect()
 
     // query database for the user object with a discordId corresponding to
     // the one stored in the session object
-    const user: IUser | null = await User.findOne({discordId: session.discordId})
+    const user: IUser | null = await User.findOne({
+        discordId: session.discordId,
+    })
         .populate({
             path: 'roles',
             populate: {
-                path: 'permissions'
-            }
-        }).exec()
+                path: 'permissions',
+            },
+        })
+        .exec()
 
-    //  if either the currently logged in user cant be found in the databse
+    //  if either the currently logged in user cant be found in the database
     // for some reason or the user has no roles at all return an exception code.
     if (!user || !(roles.length > 0)) return ResponseCode.Exception
 
