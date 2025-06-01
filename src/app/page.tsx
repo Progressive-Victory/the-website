d6a007bc8f014395bbcb887e3e4851fb4bf9c701
Tom <tjustsoup@gmail.com>
2025-06-01 12:43:08 -0400

    VolunteerMap POC - Added new component "VolunteerMap.tsx" meant to eventually replace "Volunteer" and "MapGraphic" - Added "TiltMessage" function from "Hero.tsx" to the "Message" common component - Added some additional props to "Message" for more customization   - "children", "topRightContent", "botLeftContent"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
-import { Hero, MapGraphic, MemberBanner, Volunteer } from '@/app/home'
+import { Hero, MapGraphic, MemberBanner, Volunteer, VolunteerMap } from '@/app/home'
 import { MainLayout } from '@/components/layout'
 // HomePage
 export default function Home() {
-    return (
-        <MainLayout>
-            <div className="flex flex-col items-center overflow-x-hidden">
-                <Hero />
-                <Volunteer />
-                <MapGraphic />
-                <MemberBanner />
-            </div>
-        </MainLayout>
-    )
+  return (
+    <MainLayout>
+      <div className="flex flex-col items-center overflow-x-hidden">
+        <Hero />
+        <VolunteerMap />
+        <Volunteer />
+        <MapGraphic />
+        <MemberBanner />
+      </div>
+    </MainLayout>
+  )
 }
