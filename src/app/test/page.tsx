c26c2eeda03f0f8aab232d763a7697a3d33a89ce
Wyndolin Crook <wyncrook@protonmail.com>
2025-06-01 12:06:13 -0600

    feat: api route for state counting is done.

---
+++ src/app/test/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { MainLayout } from "@/components/layout";
+import { useEffect, useState } from "react";
+
+export default function Test(){
+  const [stateCount, setStateCount] = useState<{[key: string]: number} | null>(null)
+
+  useEffect(() => {
+    const test = async () => {
+      const res = await fetch('/api/map/count')
+      setStateCount(await res.json())
+    }
+    test()
+  }, [])
+
+  return (
+    <MainLayout>
+      {stateCount ? Object.keys(stateCount).filter(x => isNaN(+x)).map(state => (
+        <div key={state}>
+          <span>{state}: </span>
+          <span>{stateCount[state]}</span>
+        </div>
+      )) : 
+        <p>loading</p>
+      }
+    </MainLayout>
+  )
+}
