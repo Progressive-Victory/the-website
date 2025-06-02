5ffa60fa73f7ee09e4b3bd2b623af0dda0e90ec8
Tom <tjustsoup@gmail.com>
2025-06-01 20:53:43 -0400

    Volunteer Cards - fix up started

--- src/app/home/Volunteer.tsx
+++ src/app/home/Volunteer.tsx
@@ -42,7 +42,7 @@ export function Volunteer() {
             <h1 className="text-4xl font-bold text-white">
                 What Can <span className="text-valencia">You </span> Do?
             </h1>
-            <div className="flex flex-col items-center justify-center gap-10 px-10 xl:flex-row">
+            <div className="flex flex-col items-center justify-center gap-10 px-10">
                 {visible &&
                     actions.map((action, index) => (
                         <Card
@@ -79,16 +79,14 @@ function Card({
                 delay,
                 ease: 'backInOut',
             }}
-            className="relative flex h-fit w-full flex-col items-center justify-start rounded-lg bg-white p-4 md:h-[275px] md:flex-row"
+            className="relative flex w-full flex-row items-center justify-start rounded-lg bg-white p-10"
         >
-            <Image src={image} alt={title} width={128} height={128} />
-            <div className="flex flex-col items-center justify-center gap-x-4">
-                <h1 className="text-center text-lg font-bold text-black-pearl-dark md:text-xl">
+            <Image src={image} alt={title} width={86} height={86} />
+            <div className="flex flex-col gap-1 px-10">
+                <h1 className="text-lg font-bold text-black-pearl-dark md:text-xl">
                     {title}
                 </h1>
-                <p className="md:text-md px-4 text-center text-sm text-black">
-                    {description}
-                </p>
+                <p>{description}</p>
             </div>
         </motion.div>
     )
--- src/app/home/VolunteerMap.tsx
+++ src/app/home/VolunteerMap.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Link, Message, TiltMessage } from '@/components/common'
 import { Map } from '@/components/Map'
+import { Volunteer } from './Volunteer'
 
 const mapText = `The PV community is constantly growing! Our members are
                     organizing in their local communities, identifying campaigns
@@ -15,29 +16,39 @@ export function VolunteerMap() {
                 <br /> Across the US
             </h1>
 
-            <TiltMessage>
-                <Message
-                    avatar="/images/pv_pride.png"
-                    avatarRounded={false}
-                    nameColor="red"
-                    username="Progressive Victory"
-                    text={mapText}
-                    topRightContent={
-                        <Link
-                            href={'/volunteer'}
-                            className="bg-valencia !px-3 !py-1.5 !text-sm"
+            <div className="grid w-full grid-cols-1 justify-center px-20 xl:grid-cols-2">
+                <div className="flex justify-center">
+                    <TiltMessage>
+                        <Message
+                            avatar="/images/pv_pride.png"
+                            avatarRounded={false}
+                            nameColor="red"
+                            username="Progressive Victory"
+                            text={mapText}
+                            topRightContent={
+                                <Link
+                                    href={'/volunteer'}
+                                    className="bg-valencia !px-3 !py-1.5 !text-sm"
+                                >
+                                    Get Involved
+                                </Link>
+                            }
+                            botDivider={true}
+                            botLeftContent={
+                                <p className="font-medium">Members: xx</p>
+                            }
                         >
-                            Get Involved
-                        </Link>
-                    }
-                    botDivider={true}
-                    botLeftContent={<p className="font-medium">Members: xx</p>}
-                >
-                    <div className="h-[450px] w-[750px] rounded-md">
-                        <Map disableInteraction />
-                    </div>
-                </Message>
-            </TiltMessage>
+                            <div className="h-[450px] w-[750px] rounded-md">
+                                <Map disableInteraction />
+                            </div>
+                        </Message>
+                    </TiltMessage>
+                </div>
+
+                <div className="flex justify-center">
+                    <Volunteer />
+                </div>
+            </div>
         </div>
     )
 }
