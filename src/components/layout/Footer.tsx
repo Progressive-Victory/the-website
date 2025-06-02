967120c930c17a4fbf1749fe5a0ec8f6f6b1b07c
Tom <tjustsoup@gmail.com>
2025-06-02 19:43:10 -0400

    Tom's Potential fix

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -12,8 +12,8 @@ const socials = [
 
 export function Footer() {
   return (
-    <div className="w-full bg-black-pearl-dark flex flex-col lg:flex-row overflow-x-hidden">
-      <div className="flex flex-col items-center lg:items-start justify-center m-6 lg:w-1/5">
+    <div className="flex w-full flex-col bg-black-pearl-dark items-center lg:flex-row">
+      <div className="m-6 flex flex-col items-center justify-center lg:w-1/5 lg:items-start">
         <Image
           src="/images/LogoFull.webp"
           alt="progressive-victory-logo"
@@ -22,63 +22,17 @@ export function Footer() {
         />
       </div>
 
-      <div className="flex flex-col items-center justify-start gap-y-4 gap-x-6 py-6 m-6 lg:w-3/5">
-        <div className="flex flex-row items-center justify-center gap-x-4 gap-y-2">
-          <Link href="/about">
-            <p className="text-white text-lg font-bold hover:underline px-8 text-center">
-              About
-            </p>
-          </Link>
-          <Link href="/volunteer">
-            <p className="text-white text-lg font-bold hover:underline px-8 text-center">
-              Volunteer
-            </p>
-          </Link>
-          <Link href="/events">
-            <p className="text-white text-lg font-bold hover:underline px-8 text-center">
-              Events
-            </p>
-          </Link>
-        </div>
+      <div className="m-4 flex flex-col items-center justify-start gap-x-6 gap-y-4 lg:w-3/5">
         <div className="flex flex-row items-center justify-center gap-x-4 gap-y-2 lg:w-full">
-          <div>
-            <a target="_blank" className="text-base text-nowrap bg-valencia px-10 py-4 rounded-full text-white font-bold hover:bg-white hover:text-black-pearl-dark transition duration-300 ease-in-out hidden xl:block"
-              href="https://secure.actblue.com/donate/pvwebsite">
-              Donate
-            </a>
-          </div>
-          <div className="flex flex-row sm:flex-wrap 3xl:flex-nowrap items-center justify-center space-x-4 gap-x-4 m-6 3xl:gap-8">
+          <div className="flex flex-row items-center justify-center gap-x-2 space-x-4 sm:flex-wrap 3xl:flex-nowrap 3xl:gap-8">
             {socials.map((social) => (
               <SocialIcon key={social} url={social} fgColor="white" />
             ))}
           </div>
-          <div>
-            <a target="_blank" className="text-base text-nowrap bg-valencia px-10 py-4 rounded-full text-white font-bold hover:bg-white hover:text-black-pearl-dark transition duration-300 ease-in-out hidden xl:block"
-              href="/volunteer">
-              Get Involved
-            </a>
-          </div>
-        </div>
-        <div className="flex flex-row items-center justify-center gap-x-4 gap-y-2">
-          <Link href="https://progressivevictory.myshopify.com/">
-            <p className="text-white text-lg font-bold hover:underline px-8 text-center">
-              Merch
-            </p>
-          </Link>
-          <Link href="https://docs.google.com/forms/d/e/1FAIpQLSdBRKV6bbxcx6HtNALWyjAwvEXbGSIG9s7iFEFlCEImVXILHA/viewform">
-            <p className="text-white text-lg font-bold hover:underline px-8 text-center">
-              Contact
-            </p>
-          </Link>
-          <Link href="/privacy">
-            <p className="text-white text-lg font-bold hover:underline px-8 text-center whitespace-nowrap">
-              Privacy Policy
-            </p>
-          </Link>
         </div>
       </div>
 
-      <div className="flex flex-col items-center lg:items-end justify-center m-6 lg:w-1/5">
+      <div className="m-6 flex flex-col items-center justify-center lg:w-1/5 lg:items-end">
         <div className="border-2 border-white p-1 text-center text-xs font-bold text-steel-blue md:mx-0 md:w-[300px]">
           PAID FOR BY PROGRESSIVE VICTORY{' '}
           <Link
