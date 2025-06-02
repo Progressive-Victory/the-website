0a26eafe0f61d0a67654930a0675e03f1f27175d
jandrews4 <jacob-andrews@uiowa.edu>
2025-06-02 09:11:08 -0500

    First push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Hero, MapGraphic, Volunteer } from '@/app/home'
 import { MainLayout } from '@/components/layout'
-
+import { MemberBanner } from '@/components/MemberBanner'
 // HomePage
 export default function Home() {
     return (
@@ -8,6 +8,7 @@ export default function Home() {
             <div className="flex flex-col items-center overflow-x-hidden">
                 <Hero />
                 <Volunteer />
+                <MemberBanner/>
                 <MapGraphic />
             </div>
         </MainLayout>
