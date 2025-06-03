38851977257947af2eda94c056b7b907214ab7a9
Wyndolin Crook <wyncrook@protonmail.com>
2025-06-02 19:23:27 -0600

    bugfix: rebased

--- src/app/volunteer/page.tsx
+++ src/app/volunteer/page.tsx
@@ -13,8 +13,6 @@ export default function Volunteer() {
     const [currentStage, setCurrentStage] = useState<string>('loading')
     const [firstName, setFirstName] = useState<string>('')
     const [lastName, setLastName] = useState<string>('')
-    const [firstName, setFirstName] = useState<string>('')
-    const [lastName, setLastName] = useState<string>('')
     const [phoneNumber, setPhoneNumber] = useState<string>('')
     const [zipCode, setZipCode] = useState<string>('')
     const [fromUS, setFromUS] = useState<boolean>(false)
