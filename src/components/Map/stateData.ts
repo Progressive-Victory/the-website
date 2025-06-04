43540844b62761e3b10727a4f2d15c81eb51a2e6
Tom <tjustsoup@gmail.com>
2025-06-04 18:12:48 -0400

    USMapLayer type fixing + pre-api call statedata formatting

--- src/components/Map/stateData.ts
+++ src/components/Map/stateData.ts
@@ -1,6 +1,12 @@
-import { GeoJSON } from 'geojson'
+import { FeatureCollection, MultiPolygon, Polygon } from 'geojson'
 
-const statesData: GeoJSON = {
+export type StateDataFeatureCollection = FeatureCollection<
+    Polygon | MultiPolygon,
+    StateDataFeatureProps
+>
+export type StateDataFeatureProps = { name: string; count?: number }
+
+const statesData: StateDataFeatureCollection = {
     type: 'FeatureCollection',
     features: [
         {
