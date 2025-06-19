1bdb00a25269d0f39e7665efd79b678b6904cde9
Ash <ash@Mac.attlocal.net>
2025-06-19 17:42:02 -0400

    Added some padding in the About page between the FAQ component and footer

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -33,7 +33,7 @@ export default function About() {
             {/* Halftone background */}
             <div className="halftone z-1 absolute inset-0 size-full opacity-10" />
 
-            <div className="z-2 relative m-auto flex min-h-screen w-full flex-col items-center justify-start gap-y-10 pt-10">
+            <div className="z-2 relative m-auto flex min-h-screen w-full flex-col items-center justify-start gap-y-10 py-10">
                 <AboutCards />
                 <FAQ />
             </div>
