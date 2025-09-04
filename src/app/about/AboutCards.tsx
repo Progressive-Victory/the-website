6bf19a27d71429eb377279e9fad86588c71f295c
Joops-Progressive-Victory <joops@progress.win>
2025-09-04 11:38:19 -0500

    updated mission statement

--- src/app/about/AboutCards.tsx
+++ src/app/about/AboutCards.tsx
@@ -3,14 +3,16 @@ const content = [
         title: 'Our',
         emphasis: 'Community',
         description: `Progressive Victory is a new kind of political community — built by the internet, for the internet. Our community is turning the tides in elections across the country. Progressive Victory seeks to challenge the traditional methods of political organizing by engaging people through online media, with a focus on building solidarity with streamers and social influencers.
-                    The PV community is based online, hosted in a dedicated Discord server. PV works closely with online content creators on Twitch and YouTube to get their engaged, dedicated audiences into the PV community Discord. There, we’re bringing new people into the political process by giving them the tactics, resources, and support to make their voices heard.
-                    Progressive Victory is constantly growing, with new members joining the community every day. The community is built upon the values of equity and inclusion, with a zero tolerance policy for hate. All members are asked to sign on to the PV rules before gaining access to the community and are upheld to the highest standards.`,
+                    \nThe PV community is based online, hosted in a dedicated Discord server. PV works closely with online content creators on Twitch and YouTube to get their engaged, dedicated audiences into the PV community Discord. There, we’re bringing new people into the political process by giving them the tactics, resources, and support to make their voices heard.
+                    \nProgressive Victory is constantly growing, with new members joining the community every day. The community is built upon the values of equity and inclusion, with a zero tolerance policy for hate. All members are asked to sign on to the PV rules before gaining access to the community and are upheld to the highest standards.`,
     },
     {
         title: 'Our',
         emphasis: 'Mission',
-        description: `Progressive Victory is committed to building our community of new-to-activism volunteers by empowering members to develop the skills and gain the knowledge needed to become lifelong activists. Progressive Victory’s unifying mission is to ensure progressive candidates are elected to positions of power and progressive policy is implemented. Over the long term, Progressive Victory aims to create a progressive power block within the Democratic Party that is too big to ignore.
-                    Progressive Victory values unity above all, and seeks to connect progressives across the ideological spectrum with the common goal of creating a more just world. Regardless of our differences, we unite over the need to take pro-democracy action in these unprecedented times.`,
+        description: `Built by the internet, for America. Progressive Victory is a new kind of political institution: seamlessly marrying the electoral impact and volunteer power of traditional progressive organizations with the culture and community of digital third places. 
+                      \nDedicated to pushing the Democratic Party to the left, we partner with left-aligned content creators to activate their disaffected, terminally online audiences converting them into lifelong, on the ground organizers. Providing the necessary structure & resources to empower viewers into volunteers, our innovative, community-first model utilizes familiar online spaces to introduce
+                      & demystify the world of political organizing. 
+                      \nProgressive Victory embraces queer & neurodivergent, new-to-activism volunteers of all backgrounds, ethnicities, and abilities. Over the long term, we aim to mainstream the online, progressive power block burgeoning within the Democratic Party and prove that it, and us, are too big to ignore.`,
     },
 ]
 
@@ -49,7 +51,7 @@ function AboutCard({
             <h1 className="py-2 text-4xl font-bold text-white">
                 {title} <span className="text-valencia">{emphasis}</span>
             </h1>
-            <p className="text-md py-4 text-left font-[500] text-white lg:text-lg">
+            <p className="text-md whitespace-pre-line py-4 text-left font-[500] text-white lg:text-lg">
                 {description}
             </p>
         </div>
