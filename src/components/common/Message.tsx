feb086fa54ba11a27de36d28173640c12775eaa9
Ultrafeeder4 <ultrafeeder4@gmail.com>
2025-09-20 16:47:52 -0400

    saved the file that I was editing

--- src/components/common/Message.tsx
+++ src/components/common/Message.tsx
@@ -96,11 +96,7 @@ export function Message({
                         src={image}
                         alt={username}
                         className="rounded-lg object-cover"
-<<<<<<< HEAD
                         style={{ objectFit: 'contain' }}
-=======
-                        style={{ objectPosition: '50% 50%' }}
->>>>>>> 1ab42013cfbaaaa9f87b9d13829af8229a610357
                         fill={true}
                         sizes="100%"
                     />
