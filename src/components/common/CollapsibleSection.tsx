53f96469f84d3d53333e520015fb1016dfeff016
Electro_593 <electro5939@gmail.com>
2025-10-02 21:20:06 -0500

    Fix useState build error

--- src/components/common/CollapsibleSection.tsx
+++ src/components/common/CollapsibleSection.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState } from 'react'
 import { FiChevronDown, FiChevronLeft } from 'react-icons/fi'
 
@@ -17,17 +19,15 @@ export function CollapsableSection({
     return (
         <section>
             {title && (
-                <>
-                    <h2 className="relative my-4 text-xl font-semibold">
-                        {title}
-                        <button
-                            className="absolute right-0 top-0 flex size-8 cursor-pointer items-center justify-center rounded-full border-2 border-gray-200 text-gray-400 hover:text-gray-500"
-                            onClick={() => setIsOpen(!isOpen)}
-                        >
-                            {isOpen ? <FiChevronDown /> : <FiChevronLeft />}
-                        </button>
-                    </h2>
-                </>
+                <h2 className="relative my-4 text-xl font-semibold">
+                    {title}
+                    <button
+                        className="absolute right-0 top-0 flex size-8 cursor-pointer items-center justify-center rounded-full border-2 border-gray-200 text-gray-400 hover:text-gray-500"
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        {isOpen ? <FiChevronDown /> : <FiChevronLeft />}
+                    </button>
+                </h2>
             )}
             {isOpen && children}
         </section>
