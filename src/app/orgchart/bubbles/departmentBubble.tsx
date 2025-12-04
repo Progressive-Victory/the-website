54421c62f0a50a864e9a810ab5efdcb79fd7844f
sylintBundy <juliangbundy@icloud.com>
2025-12-04 13:43:35 -0600

    feat: added animations to dynamic zoom

--- src/app/orgchart/bubbles/departmentBubble.tsx
+++ src/app/orgchart/bubbles/departmentBubble.tsx
@@ -1,11 +1,9 @@
 export default function DepartmentBubble({ name }: { name: string }) {
     return (
-        <div className="w-[360px] rounded-r-2xl border-4 border-amber-300 bg-amber-50 p-1 text-xl">
-            <div>
-                <p className="text-center text-xl font-extrabold text-black-pearl-dark">
-                    {name.toUpperCase()}
-                </p>
-            </div>
+        <div className="w-[360px] rounded-r-2xl border-4 border-amber-300 bg-amber-50 p-2 text-black-pearl-dark">
+            <p className="text-center text-xl font-extrabold">
+                {name.toUpperCase()}
+            </p>
         </div>
     )
 }
