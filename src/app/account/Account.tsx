2ee3d5c8e1f11ca6db28364736b82865e1d69fd9
pv-Electro593 <electro@progress.win>
2026-01-05 20:22:36 -0600

    Add account example

--- src/app/account/Account.tsx
+++ src/app/account/Account.tsx
@@ -1,18 +1,18 @@
 'use client'
 
 import { MainLayout } from '@/components/layout'
-import { hasPermission, useUser } from '@/util/hooks'
+import { hasPermission, useCurrentUser } from '@/util/hooks'
 import { InformationCircleIcon } from '@heroicons/react/24/solid'
 import { useSession, signOut } from 'next-auth/react'
 import Link from 'next/link'
-import { useEffect, useMemo } from 'react'
+import { useMemo } from 'react'
 
 export function Account() {
     const { data: session } = useSession()
-    const user = useUser()
+    const user = useCurrentUser()
 
     const AdminPanelButton = useMemo(() => {
-        if (user.data && !user.error && !user.loading) {
+        if (user.data) {
             if (hasPermission(user.data, 'Admin Panel Access')) {
                 return (
                     <Link href="/admin">
@@ -27,43 +27,33 @@ export function Account() {
         return undefined
     }, [user])
 
-    useEffect(() => {
-        // Check if the user is already on the server
-        void fetch('/api/onboarding/discord/status')
-    }, [])
+    if (!session) return null
 
-    if (!session) {
-        return null
-    } else {
-        return (
-            <MainLayout>
-                <div className="relative flex size-full min-h-screen flex-1 flex-col items-center bg-steel-blue xl:min-h-[unset]">
-                    <div className="halftone z-1 absolute left-0 top-0 size-full opacity-10" />
+    return (
+        <MainLayout>
+            <div className="relative flex size-full min-h-screen flex-1 flex-col items-center bg-steel-blue xl:min-h-[unset]">
+                <div className="halftone z-1 absolute left-0 top-0 size-full opacity-10" />
 
-                    <div className="z-2 relative mt-20 flex w-[300px] flex-col rounded-lg bg-black-pearl-dark p-4">
-                        <p className="mb-4 text-lg font-bold text-white">
-                            Account Controls
-                        </p>
-                        <div className="flex flex-row items-center justify-between">
-                            <button
-                                onClick={() =>
-                                    void signOut({ callbackUrl: '/' })
-                                }
-                                className="rounded-full bg-valencia px-4 py-2 font-bold text-white transition duration-300 ease-in-out hover:bg-white hover:text-black-pearl-dark"
-                            >
-                                Sign Out
-                            </button>
+                <div className="z-2 relative mt-20 flex w-[300px] flex-col rounded-lg bg-black-pearl-dark p-4">
+                    <p className="mb-4 text-lg font-bold text-white">
+                        Account Controls
+                    </p>
+                    <div className="flex flex-row items-center justify-between">
+                        <button
+                            onClick={() => void signOut({ callbackUrl: '/' })}
+                            className="rounded-full bg-valencia px-4 py-2 font-bold text-white transition duration-300 ease-in-out hover:bg-white hover:text-black-pearl-dark"
+                        >
+                            Sign Out
+                        </button>
 
-                            {AdminPanelButton && AdminPanelButton}
-                        </div>
-                    </div>
-                    <div className="z-2 relative mt-4 flex flex-row rounded-lg bg-black-pearl-dark p-4 text-xs text-white shadow-lg">
-                        <InformationCircleIcon className="mr-1 size-4 rounded-full bg-white text-steel-blue" />
-                        Pardon our dust while we work on this page
+                        {AdminPanelButton && AdminPanelButton}
                     </div>
                 </div>
-            </MainLayout>
-        )
-    }
+                <div className="z-2 relative mt-4 flex flex-row rounded-lg bg-black-pearl-dark p-4 text-xs text-white shadow-lg">
+                    <InformationCircleIcon className="mr-1 size-4 rounded-full bg-white text-steel-blue" />
+                    Pardon our dust while we work on this page
+                </div>
+            </div>
+        </MainLayout>
+    )
 }
-//delete this line
