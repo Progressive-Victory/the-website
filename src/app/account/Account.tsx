6606528bc6ef92e3eab7c0cac7bdf8df21125cc5
Eric Oseid <eoseid@patreon.com>
2026-01-13 22:21:43 -0800

    Setting up card image and form layout

--- src/app/account/Account.tsx
+++ src/app/account/Account.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { MainLayout } from '@/components/layout'
+import Image from 'next/image'
 
 interface AccountProps {
     firstName: string
@@ -28,6 +29,85 @@ const Account = ({
 }: AccountProps) => {
     return (
         <MainLayout>
+            <div className="halftone z-1 absolute left-0 top-0 size-full opacity-10" />
+            <div
+                className="absolute right-0 top-0 size-full lg:w-1/2 lg:translate-x-1/2"
+                style={{
+                    backgroundImage: "url('/images/blend_test.png')",
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'right',
+                    mixBlendMode: 'lighten',
+                    transform: 'scaleX(-1)',
+                }}
+            />
+            <div className="w-fill z-0 m-4 flex h-auto flex-col justify-center gap-y-4 rounded-lg bg-white p-4 shadow-md md:m-8 md:p-6">
+                <header>
+                    <p className="mx-auto text-center text-3xl font-bold text-black">
+                        Account Dashboard
+                    </p>
+                </header>
+                <div className="flex flex-row justify-between">
+                    <form className="z-0 m-4 flex h-auto w-full max-w-[500px] flex-col justify-start gap-y-4 rounded-lg bg-slate-100 p-4 shadow-md md:m-8 md:p-6">
+                        <header>
+                            <p className="text-l mx-auto text-left font-bold text-black">
+                                Account Information
+                            </p>
+                        </header>
+                        <div className="flex flex-col gap-y-2">
+                            <div className="flex flex-row justify-between">
+                                <p>Username:</p>
+                                <input></input>
+                            </div>
+                            {
+                                //<div className="flex flex-col gap-y-2">
+                                //    <p>Username:</p>
+                                //    <p>Email:</p>
+                                //    <p>Discord ID:</p>
+                                //    <p>First Name:</p>
+                                //    <p>Last Name:</p>
+                                //    <p>Preferred Name:</p>
+                                //    <p>Email:</p>
+                                //    <p>Phone Number:</p>
+                                //    <p>Date Of Birth:</p>
+                                //    <p>State:</p>
+                                //    <p>City:</p>
+                                //    <p>Zip Code:</p>
+                                //    <p>Address Line 1:</p>
+                                //    <p>Address Line 2:</p>
+                                //</div>
+                                //<div className="flex flex-col gap-y-2">
+                                //    <input></input>
+                                //    <input></input>
+                                //    <input></input>
+                                //    <input></input>
+                                //    <input></input>
+                                //    <input></input>
+                                //    <input></input>
+                                //    <input></input>
+                                //    <input></input>
+                                //    <input></input>
+                                //    <input></input>
+                                //    <input></input>
+                                //    <input></input>
+                                //    <input></input>
+                                //</div>
+                            }
+                        </div>
+                    </form>
+                    <div className="flex flex-col justify-center">
+                        <Image
+                            src="/images/membercard_front.png"
+                            alt="Front content"
+                            width={480}
+                            height={302}
+                            className="rounded-lg"
+                            priority
+                            quality={100}
+                            unoptimized
+                        />
+                    </div>
+                </div>
+            </div>
             <div>
                 <p>{firstName}</p>
                 <p>{lastName}</p>
---
+++ src/app/account/__tests__/Account.spec.tsx
@@ -0,0 +1,43 @@
+import Account from '../Account'
+import { render } from '@testing-library/react'
+import { useSession } from 'next-auth/react'
+import { describe, it, expect, vi, afterEach } from 'vitest'
+
+const testUserData = {
+    firstName: 'Eric',
+    lastName: 'Oseid',
+    city: 'Seattle',
+    state: 'WA',
+    zip: 98102,
+    addressLine1: '3100 Fairview Ave E',
+    addressLine2: 'Apt 109',
+    emailAddress: 'ericoseid@gmail.com',
+    phoneNumber: '6036678599',
+}
+
+vi.mock('next-auth/react', () => ({
+    useSession: vi.fn(),
+}))
+
+describe('Account page', () => {
+    afterEach(() => {
+        vi.resetAllMocks()
+    })
+
+    it('Does some sheeit', () => {
+        vi.mocked(useSession).mockResolvedValue({
+            update: vi.fn(),
+            data: {
+                discordId: '123',
+                accessToken: 'accessToken',
+                apiUrl: 'apiUrl',
+                expires: 'expires',
+            },
+            status: 'authenticated',
+        })
+
+        const { getByText } = render(<Account {...testUserData}></Account>)
+
+        expect(getByText('Eric')).toBeVisible()
+    })
+})
