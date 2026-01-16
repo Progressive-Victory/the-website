083bf1bbd8c8527be180abe6292228d1e9709334
pv-Electro593 <electro@progress.win>
2026-01-15 19:20:52 -0600

    Remove unused routes

--- src/app/api/map/users-count/route.ts
+++
@@ -1,9 +0,0 @@
-import { MongoUser } from '@/models/MongoUser'
-import dbConnect from '@/util/libmongo'
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-    await dbConnect()
-    const userCount = await MongoUser.countDocuments().exec()
-    return NextResponse.json(userCount)
-}
