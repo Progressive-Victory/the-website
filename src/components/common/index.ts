054a01fac13e02636a9c532cc2be9c037e1cf0ec
Benjamin Gilbert-Lif <bgl@progress.win>
2026-01-22 22:16:37 -0500

    Fixing the stupid fucking tweetcards and their stupid fucking scale factor

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,9 +1,15 @@
-export * from './buttons/button_types/OldButton'
+import { Link } from './Link'
+import { Logo } from './Logo'
+import { Button } from './buttons/button_types/OldButton'
+import { Message, TiltMessage } from './twitter_card_element/card'
+
+export { Button, Link, Logo, Message, TiltMessage }
+
 export * from './CollapsibleSection'
 export * from './ImageWithFallback'
 export * from './Link'
 export * from './Logo'
-export * from './Message'
+export * from './twitter_card_element/card'
 export * from './MultiSelect'
 export * from './Popup'
 export * from './ToolTip'
