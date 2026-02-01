cb054034179afc5f5a03fab3f8348b9718ba0141
Benjamin Gilbert-Lif <bgl@progress.win>
2026-02-01 12:49:53 -0500

    removed tilt export

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,7 +1,8 @@
 import { Logo } from './Logo'
-import { Message, TiltMessage } from './twitter_card_element/Card'
+import { Button } from './buttons/button_types/OldButton'
+import { Message } from './twitter_card_element/Card'
 
-export { Logo, Message, TiltMessage }
+export { Button, Logo, Message }
 
 export * from './CollapsibleSection'
 export * from './ImageWithFallback'
@@ -9,3 +10,4 @@ export * from './Logo'
 export * from './MultiSelect'
 export * from './Popup'
 export * from './ToolTip'
+export * from './link'
