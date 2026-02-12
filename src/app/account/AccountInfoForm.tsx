b5f091061e0379ec24ac3a29c8035cf3b9ee200e
Eric Oseid <ericoseid@pop-os.lan>
2026-02-11 20:52:14 -0800

    run prettier

--- src/app/account/AccountInfoForm.tsx
+++ src/app/account/AccountInfoForm.tsx
@@ -2,7 +2,6 @@
 
 import { StateSelector } from './StateSelector'
 import styles from './accountInfoForm.module.css'
-import { useState } from 'react'
 import {
     CheckboxField,
     DateField,
@@ -14,6 +13,7 @@ import {
     TextField,
 } from '@/components/form'
 import { dateService } from '@/services'
+import { useState } from 'react'
 
 interface MutableFields {
     firstName: string
@@ -32,90 +32,67 @@ interface AccountInformation extends MutableFields {
     discordId: string
 }
 
-
-export const AccountInfoForm = ({
-	form
-}: { form : AccountInformation }) => {
+export const AccountInfoForm = ({ form }: { form: AccountInformation }) => {
     const [updatedForm, setUpdatedForm] = useState<MutableFields>(form)
 
     const onSave = (newForm: AccountInformation) => {
         setUpdatedForm(newForm)
     }
 
-	return (
-		<div className={styles.accountInfoBackground}>
-		<Form<AccountInformation> 
-			form={ updatedForm }
-			title="Account Information"
-			onUpdate={() => {}}
-			onSave={onSave}
-		>
-            <FormGroup >
-				<TextField
-				    label="Discord Username"
-					field="discordUsername"
-				    readonly
-				/>
-				<TextField
-				    label="Discord ID"
-					field="discordId"
-				    readonly
-				/>
-				<TextField
-				    label="First Name"
-					field="firstName"
-				/>
-				<TextField
-				    label="Last Name"
-					field="lastName"
-				/>
-                <DateField
-                    label="Date of Birth"
-                    getter={(form) =>
-                        dateService.isValid(form.birthdate)
-                            ? new Date(
-                                  dateService.toISODateString(
-                                      form.birthdate
-                                  )!
-                              )
-                            : null
-                    }
-                    field="birthdate"
-                    format={{
-                        timeZone: 'UTC',
-                        dateStyle: 'medium',
-                    }}
-				/>
-				<TextField
-					label="State"
-					field="state"
-				/>
-				<TextField
-					label="County"
-					field="county"
-				/>
-				<TextField
-					label="City"
-					field="city"
-				/>
-                <TextField<User>
-                    label="Zip Code"
-                    getter={(form) =>
-                        form.zip.toString().padStart(5, '0').slice(-5)
-                    }
-                    setter={(form, field) => ({
-                        ...form,
-						zip: field.replace(/[^\d]/, '').padStart(5, '0').slice(-5),
-                    })}
-                />
-                <TextField label="Email" field="emailAddress"/>
-                <TextField
-                    label="Phone Number"
-                    field="phoneNumber"
-                />
-			</FormGroup >
-		</Form>
-		</div>
-	)
+    return (
+        <div className={styles.accountInfoBackground}>
+            <Form<AccountInformation>
+                form={updatedForm}
+                title="Account Information"
+                onUpdate={() => {}}
+                onSave={onSave}
+            >
+                <FormGroup>
+                    <TextField
+                        label="Discord Username"
+                        field="discordUsername"
+                        readonly
+                    />
+                    <TextField label="Discord ID" field="discordId" readonly />
+                    <TextField label="First Name" field="firstName" />
+                    <TextField label="Last Name" field="lastName" />
+                    <DateField
+                        label="Date of Birth"
+                        getter={(form) =>
+                            dateService.isValid(form.birthdate)
+                                ? new Date(
+                                      dateService.toISODateString(
+                                          form.birthdate
+                                      )!
+                                  )
+                                : null
+                        }
+                        field="birthdate"
+                        format={{
+                            timeZone: 'UTC',
+                            dateStyle: 'medium',
+                        }}
+                    />
+                    <TextField label="State" field="state" />
+                    <TextField label="County" field="county" />
+                    <TextField label="City" field="city" />
+                    <TextField<User>
+                        label="Zip Code"
+                        getter={(form) =>
+                            form.zip.toString().padStart(5, '0').slice(-5)
+                        }
+                        setter={(form, field) => ({
+                            ...form,
+                            zip: field
+                                .replace(/[^\d]/, '')
+                                .padStart(5, '0')
+                                .slice(-5),
+                        })}
+                    />
+                    <TextField label="Email" field="emailAddress" />
+                    <TextField label="Phone Number" field="phoneNumber" />
+                </FormGroup>
+            </Form>
+        </div>
+    )
 }
-
