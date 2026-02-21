8403614c83a4113a6a1b6d56a873527cd8a31164
Benjamin Gilbert-Lif <bgl@progress.win>
2026-02-21 15:56:24 -0500

    wooo

--- src/app/admin/panels/members/page.tsx
+++ src/app/admin/panels/members/page.tsx
@@ -2,9 +2,10 @@
 
 import styles from './page.module.css'
 import { DonorView } from './panel_views/DonorView'
+import { HistoryView } from './panel_views/HistoryView'
 import { MemberView } from './panel_views/MemberView'
 import { ListElement, List } from '@/app/admin/layout/List'
-import { CollapsibleSection, ImageWithFallback } from '@/components/common'
+import { ImageWithFallback } from '@/components/common'
 import { FormState } from '@/components/common/forms'
 import { Tab } from '@/components/common/tab_bar/Tab'
 import { TabBar } from '@/components/common/tab_bar/TabBar'
@@ -37,7 +38,6 @@ import {
     useQueryClient,
     UseQueryResult,
 } from '@tanstack/react-query'
-import cx from 'classnames'
 import { useCallback, useMemo, useState } from 'react'
 import { PulseLoader } from 'react-spinners'
 
@@ -105,6 +105,7 @@ export default function Page() {
     const formZip = formState?.editing
         ? formState?.form?.location?.zip
         : userQuery.data?.location?.zip
+
     const locationQuery = useQuery({
         queryKey: [`/locations/${formZip}`],
         queryFn:
@@ -235,6 +236,7 @@ export default function Page() {
         }
 
         setSelectedId(value.id)
+        setSelectedHistory(null)
     }
 
     const handleSave = (user: User) => {
@@ -396,7 +398,6 @@ export default function Page() {
                                 selectedId={selectedId}
                                 user={userQuery.data}
                                 selectedHistory={selectedHistory}
-                                onSelectHistory={handleSelectHistory}
                                 formState={formState}
                                 setFormState={setFormState}
                                 saving={updateMutation.isPending}
@@ -426,6 +427,16 @@ export default function Page() {
                                 onDonorSearch={onDonorSearch}
                                 renderDonorItem={renderDonorItem}
                                 handleDeleteDonorItem={handleDeleteDonorItem}
+                            />
+                        </Tab>
+
+                        <Tab key="history" label="History">
+                            <HistoryView
+                                selectedId={selectedId}
+                                user={userQuery.data}
+                                selectedHistory={selectedHistory}
+                                onSelectHistory={handleSelectHistory}
+                                isRefetching={userQuery.isRefetching}
                             />
                         </Tab>
                     </TabBar>
---
+++ src/app/admin/panels/members/panel_views/HistoryView.tsx
@@ -0,0 +1,105 @@
+'use client'
+
+import styles from './HistoryView.module.css'
+import { CollapsibleSection } from '@/components/common'
+import { FormGroupProps } from '@/components/common/forms'
+import { UpdateHistory, User } from '@/contracts/data'
+import cx from 'classnames'
+
+export interface HistoryViewProps {
+    selectedId: number
+    user: User
+    selectedHistory: UpdateHistory<User> | null
+    onSelectHistory: (update: UpdateHistory<User> | null) => void
+    isRefetching: boolean
+}
+
+export function HistoryView({
+    selectedId,
+    user,
+    selectedHistory,
+    onSelectHistory,
+    isRefetching,
+}: HistoryViewProps) {
+    if (selectedId == null) return null
+
+    if (isRefetching) {
+        return (
+            <div className={styles.historyContainer}>
+                <div className={styles.historyEntry}>Refreshing…</div>
+            </div>
+        )
+    }
+
+    if (!user?.history?.length) {
+        return (
+            <div className={styles.historyContainer}>
+                <div className={styles.historyEntry}>No history found</div>
+            </div>
+        )
+    }
+
+    return (
+        <AccountHistoryField
+            title="Account History"
+            history={user.history}
+            selected={selectedHistory}
+            onSelect={onSelectHistory}
+        />
+    )
+}
+
+interface AccountHistoryFieldProps extends FormGroupProps<User> {
+    history?: UpdateHistory<User>[]
+    selected: UpdateHistory<User> | null
+    onSelect: (update: UpdateHistory<User> | null) => void
+}
+
+function AccountHistoryField({
+    title,
+    defaultCollapsed,
+    history,
+    selected,
+    onSelect,
+}: AccountHistoryFieldProps) {
+    const value = (history ?? []).sort(
+        (a, b) =>
+            b.historyWhenUpdatedUtc.getTime() -
+            a.historyWhenUpdatedUtc.getTime()
+    )
+
+    return (
+        <div className={styles.section}>
+            <CollapsibleSection
+                title={title}
+                initialOpenState={!defaultCollapsed}
+            >
+                <div className={styles.historyContainer}>
+                    {value.map((update, i) => (
+                        <div key={i}>
+                            <button
+                                onClick={() => onSelect(i ? update : null)}
+                                className={cx(
+                                    styles.historyEntry,
+                                    (selected?.historyId == update.historyId ||
+                                        (!i && !selected)) &&
+                                        styles.historyEntrySelected
+                                )}
+                            >
+                                <span color="#4b5563">{`${update.historyType == 'I' ? 'Created' : 'Updated'} at `}</span>
+                                <span className={styles.historyEntryDate}>
+                                    {update.historyWhenUpdatedUtc.toLocaleString()}
+                                </span>
+                                <span color="#4b5563">{' by '}</span>
+                                <code>
+                                    {update.email ?? 'deleted user'}#
+                                    {update.id.toString()}
+                                </code>
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </CollapsibleSection>
+        </div>
+    )
+}
