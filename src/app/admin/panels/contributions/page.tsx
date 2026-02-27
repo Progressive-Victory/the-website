ea8c34ed413bf815a36268529777202c96e3ff45
sH3llH0und <wyn@progress.win>
2026-02-26 18:17:07 -0700

    added custom fields to contributions page

--- src/app/admin/panels/contributions/page.tsx
+++ src/app/admin/panels/contributions/page.tsx
@@ -206,6 +206,15 @@ export default function Page() {
                                 field="contributionForm"
                             />
                             <TextField label="Form Kind" field="kind" />
+                            {contributionQuery.data?.customFields?.map(
+                                (field) => (
+                                    <TextField
+                                        key={field.label}
+                                        label={field.label}
+                                        getter={() => field.answer}
+                                    />
+                                )
+                            )}
                         </FormGroup>
                     </Form>
                 )}
