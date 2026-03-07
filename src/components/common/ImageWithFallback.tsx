d23d6559058a3945ca02c2f61eb5307511422c70
Benjamin Gilbert-Lif <bgl@progress.win>
2026-03-07 18:25:21 -0500

    Revert "fixing"
    This reverts commit c622435750931ca95cda100b732afbdd50de4195.

--- src/components/common/ImageWithFallback.tsx
+++ src/components/common/ImageWithFallback.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import styles from './ImageWithFallback.module.css'
+import cx from 'classnames'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
@@ -22,59 +22,22 @@ export function ImageWithFallback({
     className,
 }: ImageWithFallbackProps) {
     const [hasErrored, setHasErrored] = useState(false)
-    const withExternalClass = (baseClass: string) =>
-        [baseClass, className].filter(Boolean).join(' ')
 
     useEffect(() => {
         setHasErrored(false)
     }, [src, useFallback])
 
-    const shouldUsePlaceholder = hasErrored || useFallback
-
-    if (shouldUsePlaceholder) {
-        const markSize = Math.max(
-            14,
-            Math.round(Math.min(width, height) * 0.52)
-        )
-
-        return (
-            <div
-                role="img"
-                aria-label={alt}
-                style={{
-                    width,
-                    height,
-                    aspectRatio: '1 / 1',
-                }}
-                className={withExternalClass(
-                    `${styles.circleBase} ${styles.placeholder}`
-                )}
-            >
-                <span
-                    aria-hidden="true"
-                    className={styles.placeholderMark}
-                    style={{
-                        fontSize: markSize,
-                    }}
-                >
-                    !
-                </span>
-            </div>
-        )
-    }
-
     return (
         <Image
-            src={src}
+            src={
+                hasErrored || useFallback
+                    ? 'https://dummyjson.com/image/100x100/e8e0e0/d0c8c8?text=!&fontFamily=Poppins'
+                    : src
+            }
             alt={alt}
             width={width}
             height={height}
-            style={{
-                aspectRatio: '1 / 1',
-            }}
-            className={withExternalClass(
-                `${styles.circleBase} ${styles.image}`
-            )}
+            className={cx('aspect-square rounded-full object-cover', className)}
             onError={() => setHasErrored(true)}
         />
     )
