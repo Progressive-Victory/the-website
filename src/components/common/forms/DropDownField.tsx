bdc6077069454e2c9e38f5c7b27c865f8366328b
sH3llH0und <wyn@progress.win>
2026-03-31 22:53:51 +0000

    Feature/member fulfillment section and drop downs (#375)
    * for shell
    
    * electro please bully my code
    
    * drop downs fully implemented
    
    * removed superfluous console logs
    
    * fixed lint errors
    
    ---------
    
    Co-authored-by: Benjamin Gilbert-Lif <bgl@progress.win>

---
+++ src/components/common/forms/DropDownField.tsx
@@ -0,0 +1,55 @@
+import { FormField, FormFieldProps, useConfigure } from './FormField'
+import styles from './FormField.module.css'
+import formFieldStyles from '@/components/common/forms/FormField.module.css'
+import { ChangeEvent, useCallback, useMemo } from 'react'
+
+export interface DropDownProps<T, F> extends FormFieldProps<T, F> {
+    options: { value: string | number; label: string }[]
+}
+
+export function DropDownField<T>(
+    props: DropDownProps<T, string | number | null | undefined>
+) {
+    const { getter, onChange, readonly } = useConfigure(
+        props,
+        useCallback(
+            (field: string | number | null | undefined) =>
+                !props.required || !!field,
+            [props.required]
+        )
+    )
+
+    const value = getter(props.dynamic!.form) ?? ''
+
+    const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        onChange(event.target.value)
+    }
+
+    const selectedLabel = useMemo(
+        () => props.options.find((x) => x.value === value)?.label,
+        [value, props.options]
+    )
+
+    return (
+        <FormField {...props}>
+            {readonly ? (
+                <div className={styles.readonly}>{selectedLabel}</div>
+            ) : (
+                <select
+                    id={props?.id}
+                    name={props.label}
+                    value={value}
+                    onChange={handleChange}
+                    disabled={props.dynamic?.saving == true}
+                    className={formFieldStyles.textField}
+                >
+                    {props.options.map((option) => (
+                        <option key={option.value} value={option.value}>
+                            {option.label}
+                        </option>
+                    ))}
+                </select>
+            )}
+        </FormField>
+    )
+}
--- src/contracts/requests/UpdateUserRequest.ts
+++ src/contracts/requests/UpdateUserRequest.ts
@@ -1,26 +1,41 @@
-import { zOnboardingStage, zUserStatus } from '../data'
 import { zUpdateUserAddressRequest } from './UpdateUserAddressRequest'
+import {
+    zOnboardingStage,
+    zUserStatus,
+    zShirtSize,
+    zMembershipDeliverableStatus,
+    zMembershipFulfillmentStatus,
+} from '@/contracts/data'
 import z from 'zod'
 
 export const zUpdateUserRequest = z
     .object({
-        email: z.string().max(100).nonempty().nullable().optional(),
-        phone: z.string().max(15).nonempty().nullable().optional(),
-        preferredName: z.string().max(100).nonempty().nullable().optional(),
-        firstName: z.string().max(100).nonempty().nullable().optional(),
-        lastName: z.string().max(100).nonempty().nullable().optional(),
-        birthdate: z.coerce.date().nullable().optional(),
+        email: z.string().max(100).nonempty().nullish(),
+        phone: z.string().max(15).nonempty().nullish(),
+        preferredName: z.string().max(100).nonempty().nullish(),
+        firstName: z.string().max(100).nonempty().nullish(),
+        lastName: z.string().max(100).nonempty().nullish(),
+        birthdate: z.coerce.date().nullish(),
+        zipCode: z.number().nullish(),
         address: zUpdateUserAddressRequest.optional(),
 
         acceptedAlerts: z.boolean().optional(),
         verified: z.boolean().optional(),
         onboardingStage: zOnboardingStage.optional(),
-        lastSmsCode: z.number().nullable().optional(),
-        lastSmsCodeSendTimeUtc: z.coerce.date().nullable().optional(),
+        lastSmsCode: z.number().nullish(),
+        lastSmsCodeSendTimeUtc: z.coerce.date().nullish(),
         status: zUserStatus.optional(),
 
-        joinedAtUtc: z.coerce.date().nullable().optional(),
-        completedIntakeUtc: z.coerce.date().nullable().optional(),
+        joinedAtUtc: z.coerce.date().nullish(),
+        completedIntakeUtc: z.coerce.date().nullish(),
+
+        membershipCardStatus: zMembershipDeliverableStatus.optional(),
+        membershipMerchStatus: zMembershipDeliverableStatus.optional(),
+        shirtSize: zShirtSize.nullish(),
+        duesPayingMember: z.boolean().optional(),
+        membershipFulfillmentStatus: zMembershipFulfillmentStatus.nullish(),
+        nameConfirmed: z.boolean().optional(),
+        addressConfirmed: z.boolean().optional(),
 
         aliases: z.array(z.string().max(100).nonempty()).optional(),
         roles: z.array(z.number()).optional(),
