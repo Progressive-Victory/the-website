4545c7a4614910dec2c146aeb24f3f2c57ced1aa
MaxDobbs32 <dobbsma@proton.me>
2026-04-20 21:00:11 -0400

    Added NOT_CITIZEN to OnboardingStage

--- src/contracts/data/OnboardingStage.ts
+++ src/contracts/data/OnboardingStage.ts
@@ -14,6 +14,9 @@ export enum OnboardingStage {
     // The user is under 18 and isn't allowed to join the server.
     UNDERAGE = 'underage',
 
+    // The user is neither a US citizen nor resident and isn't allowed to join the server.
+    NOT_CITIZEN = 'not_citizen'
+
     // The user has joined the server successfully.
     JOINED = 'joined',
 }
