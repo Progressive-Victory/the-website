a4a7db38001a857dd442e89751cb0cddd95bbea8
sH3llH0und <wyn@progress.win>
2026-04-26 10:55:50 -0600

    removed console log

--- src/app/admin/panels/members/panel_views/HistoryView.tsx
+++ src/app/admin/panels/members/panel_views/HistoryView.tsx
@@ -354,8 +354,6 @@ function UnifiedHistoryField({
                         onSelectAccountHistory(null)
                     }
 
-                    console.log(item.update.historyId ?? i)
-
                     return (
                         <div key={`${item.kind}-${item.update.historyId ?? i}`}>
                             <button
