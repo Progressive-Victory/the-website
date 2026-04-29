000a4f54478168d961bb0d70802a725b7f42c8a3
MaxDobbs32 <dobbsma@proton.me>
2026-04-29 13:47:09 -0400

    Added spacing to bottom of message

--- src/app/volunteer/NotCitizenStage.tsx
+++ src/app/volunteer/NotCitizenStage.tsx
@@ -2,7 +2,10 @@ import { SupportNote } from '.'
 
 export function NotCitizenStage() {
     return (
-        <div className="flex min-h-[200px] max-w-[40vw] flex-col items-center justify-center p-4">
+        <div
+            className="flex min-h-[200px] max-w-[40vw] flex-col items-center justify-center p-4"
+            style={{marginBottom: 30 + 'px'}}
+        >
             <p className="mb-3 mt-6 text-center text-lg font-bold text-white">
                 Sorry! You have to be a citizen and/or resident of the United
                 States to volunteer with Progressive Victory.
