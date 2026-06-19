f8a649d8d4107ecb0afed87c8352bcfb70170d3f
sH3llH0und <wyn@progress.win>
2026-06-18 21:28:58 -0600

    cum

--- src/components/Map/index.ts
+++ src/components/Map/index.ts
@@ -1,3 +1,12 @@
+'use client'
+
+import dynamic from 'next/dynamic'
+
+const Map = dynamic(() => import('./Map.client'), { ssr: false })
+const StateMap = dynamic(() => import('./StateMap'), { ssr: false })
+
+export { Map, StateMap }
+
 export * from './constants'
 export * from './Map.client'
 export * from './ResponsiveFit'
