9a0bbcb84bbe3565868628a60e050b38cf0f8a69
Luke Norman <60233616+LNorman8@users.noreply.github.com>
2026-06-23 14:58:29 -0600

    Why did the merge do that.
    "0 warnings"

--- src/app/admin/panels/members/panel_views/DonorView.tsx
+++ src/app/admin/panels/members/panel_views/DonorView.tsx
@@ -8,20 +8,15 @@ import {
     FormGroup,
     TextField,
 } from '@/components/common/forms'
-import { FetchError } from '@/models'
-import type { UseQueryResult } from '@tanstack/react-query'
-import cx from 'classnames'
-import { motion } from 'motion/react'
-import Link from 'next/link'
 import {
     ActBlueContribution,
     ActBlueContributionCustomField,
     ActBlueDonor,
     ActBlueLineitem,
     User,
-} from '@/contracts/data'
-import type { SearchRequest } from '@/contracts/requests'
-import type { PaginatedResponse } from '@/contracts/responses'
+} from 'pv-contracts/data'
+import type { SearchRequest } from 'pv-contracts/requests'
+import type { PaginatedResponse } from 'pv-contracts/responses'
 import { FetchError } from '@/models'
 import type { UseQueryResult } from '@tanstack/react-query'
 import cx from 'classnames'
