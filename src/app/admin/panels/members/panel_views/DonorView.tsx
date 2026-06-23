f5afb62d7bc0b9653191ee58fb713444e02ffb88
Luke Norman <60233616+LNorman8@users.noreply.github.com>
2026-06-23 15:49:07 -0600

    Fixed 2 `preserve-manual-memoization` errors. 0 Problems, 7 `set-state` ignored
    The compiler failing to use our manual memoization is a genuine issue, and is something we wouldn't want to miss

--- src/app/admin/panels/members/panel_views/DonorView.tsx
+++ src/app/admin/panels/members/panel_views/DonorView.tsx
@@ -8,6 +8,11 @@ import {
     FormGroup,
     TextField,
 } from '@/components/common/forms'
+import { FetchError } from '@/models'
+import type { UseQueryResult } from '@tanstack/react-query'
+import cx from 'classnames'
+import { motion } from 'motion/react'
+import Link from 'next/link'
 import {
     ActBlueContribution,
     ActBlueContributionCustomField,
@@ -17,11 +22,6 @@ import {
 } from 'pv-contracts/data'
 import type { SearchRequest } from 'pv-contracts/requests'
 import type { PaginatedResponse } from 'pv-contracts/responses'
-import { FetchError } from '@/models'
-import type { UseQueryResult } from '@tanstack/react-query'
-import cx from 'classnames'
-import { motion } from 'motion/react'
-import Link from 'next/link'
 import React, { ChangeEvent, useMemo, useState } from 'react'
 
 export interface DonorViewProps {
