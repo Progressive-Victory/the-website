a634efa6d7a4be79abdfb0c66e9578d089464aa1
Luke <60233616+LNorman8@users.noreply.github.com>
2026-06-26 01:41:53 +0000

    Clean Name formatting changes (#460)
    * Clean Name formatting changes
    
    * Add detail to comment
    
    * Remove unnecessary

--- src/app/admin/panels/members/panel_views/MemberView.tsx
+++ src/app/admin/panels/members/panel_views/MemberView.tsx
@@ -91,6 +91,7 @@ export interface MemberViewProps {
     setFormState?: (next: FormState<User> | null) => void
 
     saving: boolean
+    editing: boolean
     isInvalid: boolean
     roles: Role[]
     roleOptions: { value: number; label: string }[]
@@ -106,6 +107,7 @@ export function MemberView({
     setFormState,
     saving,
     isInvalid,
+    editing,
     roles,
     roleOptions,
     makeFormTitle,
@@ -149,8 +151,21 @@ export function MemberView({
                     field="preferredName"
                     deprecated
                 />
-                <TextField label="First Name" field="firstName" />
-                <TextField label="Last Name" field="lastName" />
+                {editing ? (
+                    <TextField label="First Name" field="firstName" />
+                ) : (
+                    <TextField<User>
+                        label="Full Name"
+                        getter={(user) =>
+                            `${user.firstName ?? ''} ${user.lastName ?? ''}`.trim()
+                        }
+                        readonly
+                    />
+                )}
+                {/* 6/25/36 - There is no way to merge this conditional in with the above trinary. 
+                    The obvious solution (fragment) causes the page to break for unknowable reasons. 
+                    Check website PR #493 to see details of how each solution breaks */}
+                {editing && <TextField label="Last Name" field="lastName" />}
                 <DateField<User>
                     label="Date of Birth"
                     getter={(form) =>
