963ecfbf5ad5b192e6ec6972738d01a51f81af3f
Ash <ashleylstringer@gmail.com>
2026-07-16 20:47:44 -0400

    basic version of new constructOrgChart iteration mostly functional

--- src/app/orgchart/app.tsx
+++ src/app/orgchart/app.tsx
@@ -8,7 +8,12 @@ import {
     PositionBubble,
 } from './components/position'
 import { TeamNode, TeamNodeData } from './components/team'
-import { BuildGraphNodes, Committees } from './data/constructOrgGraph'
+import { posHierarchy } from './data/DummyNodes'
+import {
+    BuildGraphNodes,
+    Committees,
+    testNewGraphBuilder,
+} from './data/constructOrgGraph'
 import { orgchartData } from './data/orgchartGraphData'
 import dagre from '@dagrejs/dagre'
 import {
@@ -61,7 +66,11 @@ const GetElements = (nodes: Node[], edges: Edge[], direction = 'TB') => {
     return { nodes: newNodes, edges }
 }
 
-const { initialNodes, initialEdges } = BuildGraphNodes(orgchartData)
+//const { initialNodes, initialEdges } = BuildGraphNodes(orgchartData)
+//console.log(initialNodes)
+//testNewGraphBuilder(posHierarchy)
+const { initialNodes, initialEdges } = testNewGraphBuilder(posHierarchy)
+//console.log(initialNodes)
 
 const nodeTypes = {
     pos: PositionNode,
