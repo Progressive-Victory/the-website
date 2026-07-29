8218d8d1e0dd20795e0524f3c00c261afd823ed1
zoe2276 <zoe@zlynn.dev>
2026-07-29 19:16:59 +0000

    prettier said it was ugly so i consoled it to make it feel better

--- src/contracts/data/DiscordEventStatus.ts
+++ src/contracts/data/DiscordEventStatus.ts
@@ -1,10 +1,10 @@
-import z from 'zod';
+import z from 'zod'
 
 export enum DiscordEventStatus {
-	Scheduled = 1,
-	Active = 2,
-	Completed = 3,
-	Cancelled = 4,
+    Scheduled = 1,
+    Active = 2,
+    Completed = 3,
+    Cancelled = 4,
 }
 
-export const zDiscordEventStatus = z.enum(DiscordEventStatus);
+export const zDiscordEventStatus = z.enum(DiscordEventStatus)
