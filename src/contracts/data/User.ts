457cbaad4c47a9ec695ee381dbe1f3c43f94dce7
Joseph <josephbmelbergthird@outlook.com>
2026-07-29 17:21:27 -0500

    whoops:

--- src/contracts/data/User.ts
+++ src/contracts/data/User.ts
@@ -73,7 +73,6 @@ const zBaseUser = z.object({
     membershipFulfillmentStatus: zMembershipFulfillmentStatus.nullable(),
     nameConfirmed: z.boolean(),
     addressConfirmed: z.boolean(),
-    membershipBenefitEligible: z.boolean(),
 
     aliases: z.array(z.string()).optional(),
     roles: z.array(zRole).optional(),
