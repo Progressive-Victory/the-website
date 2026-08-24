c07f12be3ef43f7b0203d6c0156e2b1d8d4d7708
Benjamin Gilbert-Lif <bgl@progress.win>
2026-08-24 12:14:49 -0400

    implementing base NavigationStack Component

--- src/app/volunteer_dashboard/layout.tsx
+++ src/app/volunteer_dashboard/layout.tsx
@@ -2,14 +2,23 @@
 
 import styles from './admin.module.css'
 import { ProtectedPage } from '@/components/ProtectedPage'
+import { NavigationStack } from '@/components/common/navigation_stack/NavigationStack'
 import { Header } from '@/components/layout/Header'
 
 export default function Layout(/*({ children }: { children: React.ReactNode }) */) {
+    // const isPanelSelected = pathname.startsWith('/volunteer_dashboard/panels/')
+
     return (
         <ProtectedPage requiredRoles={['Superadmin']}>
             <div className={styles.root}>
                 <Header />
-                Placeholder for later in PR
+                <NavigationStack
+                    className={styles.navigationStack}
+                    // isSelected={isPanelSelected}
+                    sidebar={<div>Place Holder Sidebar content</div>}
+                    detail={<div>Place Holder Detail content</div>}
+                    unSelected={<div>Place Holder Unselected content</div>}
+                />
             </div>
         </ProtectedPage>
     )
