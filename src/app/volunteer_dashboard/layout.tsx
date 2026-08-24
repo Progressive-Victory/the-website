6f17a84944698c48aa223b52463785549ee6b35b
Benjamin Gilbert-Lif <bgl@progress.win>
2026-08-24 14:39:52 -0400

    implementing base NavigationStack Component (#563)
    * implementing base NavigationStack Component
    
    * implementing folder structure for clarity of how it will work
    
    * addressing electro's comments
    
    * i forgot to change
    
    * removing placeholders

--- src/app/volunteer_dashboard/layout.tsx
+++ src/app/volunteer_dashboard/layout.tsx
@@ -2,14 +2,20 @@
 
 import styles from './admin.module.css'
 import { ProtectedPage } from '@/components/ProtectedPage'
+import { NavigationStack } from '@/components/common/navigation_stack/NavigationStack'
 import { Header } from '@/components/layout/Header'
 
-export default function Layout(/*({ children }: { children: React.ReactNode }) */) {
+export default function Layout() {
     return (
         <ProtectedPage requiredRoles={['Superadmin']}>
             <div className={styles.root}>
                 <Header />
-                Placeholder for later in PR
+                <NavigationStack
+                    className={styles.navigationStack}
+                    sidebar={<div>Place Holder Sidebar content</div>}
+                    detail={<div>Place Holder Detail content</div>}
+                    unselected={<div>Place Holder Unselected content</div>}
+                />
             </div>
         </ProtectedPage>
     )
