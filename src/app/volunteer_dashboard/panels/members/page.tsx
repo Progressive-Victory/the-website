fcb982e7cf4349b3e6ea7573084e160df2718369
prubanau16 <paulrubanau@gmail.com>
2026-09-14 18:19:53 -0500

    remove padding volunteer dashboard

--- src/app/volunteer_dashboard/panels/members/page.tsx
+++ src/app/volunteer_dashboard/panels/members/page.tsx
@@ -544,7 +544,7 @@ export default function Page() {
                     locationQuery.data?.state,
                 zip:
                     orNull(user.address.zip?.trim()) ??
-                    locationQuery.data?.zip?.toString().padStart(5, '0'),
+                    locationQuery.data?.zip?.toString(),
             }
 
             const oldAddress = userQuery.data?.address ?? null
